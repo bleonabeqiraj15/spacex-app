f8da880943b19c6e34dc86e33d8db98039393bb8
bleonabeqiraj <bleona.beqirajj@gmail.com>
2022-01-17 23:18:49 +0100

    rockets

--- src/components/molecules/RocketsInfo.js
+++ src/components/molecules/RocketsInfo.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { AiOutlineArrowLeft } from 'react-icons/ai';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
 
 const RocketsInfo = ({ rocket }) => {
 
@@ -15,47 +19,29 @@ const RocketsInfo = ({ rocket }) => {
     };
 
     return (
-        <div>
-            <div style={imgStyle}>
-                <div className="pos-link">
-                    <Link to='/rockets' className="link" style={{ color: '#000' }}>Go back <AiOutlineArrowLeft /></Link>
-                </div>
 
-                <div className="roc-details">
-                    <div>
-                        {/* <h1>{rocket.name.toUpperCase()}</h1> */}
-                        <h3>{country}</h3>
-                        <h2>{name.toUpperCase()}</h2>
-                        <div className="falcon">
-
-                        </div>
-                    </div>
-                    {/* <div className="rocketDetails">
-                        <div className="text-shadow">
-                            <div className="rocketParameters">
-                                <h3>{rocket.height.meters}m</h3>
-                            </div>
-                            <h6>height</h6>
-                        </div>
-                        <div className="text-shadow">
-                            <div className="rocketParameters">
-                                <h3>{rocket.diameter.meters}m</h3>
-                            </div>
-                            <h6>diameter</h6>
-                        </div>
-                        <div className="text-shadow">
-                            <div className="rocketParameters">
-                                <h3>{rocket.mass.kg}kg</h3>
-                            </div>
-                            <h6>mass</h6>
-                        </div>
-                    </div> */}
-                </div>
+        <div style={imgStyle}>
+            <div className="pos-link">
+                <Link to='/rockets' className="link" style={{ color: '#000' }}>Go back <AiOutlineArrowLeft /></Link>
             </div>
 
-            <div className="test">
-
-            </div>
+            <Grid container style={{ p: 2, margin: 'auto', maxWidth: 400, flexGrow: 1, padding: "100px 30px" }} className='roc-details'>
+                <Grid item xs={12} sm container>
+                    <Grid item xs container spacing={2}>
+                        <Grid item xs>
+                            <Typography variant="h2">
+                                {name}
+                            </Typography>
+                            <Typography variant="body1">
+                                {description}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                ID: 1030114
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         </div>
     )
 }
